141f7b5defe01da1dd119310275221d1902160af
Behzad Chizari <behzad75chizari@gmail.com>
2017-08-06 14:48:17 +0430

    Edit

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,43 @@
 // Add custom js here
 /* Open the sidenav */
+$(document).ready(function () {
+    var acc = $("#mySidenav a");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
+    }
+});
+$("#mySidenav a").click(function () {
+    debugger;
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.animate({display:"none"},2000);
+    } else {
+        panel.animate({ display: "block" },2000);
+    }
+});
+
+
 var button_clicked = false;
 function toggleNav() {
-        debugger; 
     if (!button_clicked) {
         $("#mySidenav").css('width', '100% ');
-        $("#mySidenav a").animate({opacity:'1'},200);
-        //$("#mySidenav a").css('opacity', '1');
+        $("#mySidenav a").css('opacity', '1');
     }
     if (button_clicked) {
-        //$("#mySidenav a").animate({opacity:'0'},500);
         $("#mySidenav a").css('opacity', '0');
         $("#mySidenav").css('width', '0');
     }
