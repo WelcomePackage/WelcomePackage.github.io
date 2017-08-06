21f0490b87d81c4d3faee6994fdb7d5ee568525b
Behzad Chizari <behzad75chizari@gmail.com>
2017-08-06 14:17:28 +0430

    edit

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,14 @@
 /* Open the sidenav */
 var button_clicked = false;
 function toggleNav() {
+        debugger; 
     if (!button_clicked) {
         $("#mySidenav").css('width', '100% ');
-        $("#mySidenav a").css('opacity', '1');
+        $("#mySidenav a").animate({opacity:'1'},200);
+        //$("#mySidenav a").css('opacity', '1');
     }
     if (button_clicked) {
+        //$("#mySidenav a").animate({opacity:'0'},500);
         $("#mySidenav a").css('opacity', '0');
         $("#mySidenav").css('width', '0');
     }
