d9d4710a3d242f678b8a9f8ea33ef69b59288311
Behzad Chizari <behzad75chizari@gmail.com>
2017-08-09 10:05:54 +0430

    Edit

--- js/main.js
+++ js/main.js
@@ -27,4 +27,36 @@ $(document).ready(function () {
         $('html,body').animatescroll();
 
     });
+    var quickAccessWidth;
+    var quickAccessOpened = false;
+    
+    $('.quick-access-icon').click(function () {
+        var windowWidth = $(window).width();
+        if (windowWidth <= 768) {
+            quickAccessWidth = '80%';
+        }
+        else if (windowWidth > 768 && windowWidth <= 992) {
+            quickAccessWidth = '60%';
+        }
+        else if (windowWidth > 992 && windowWidth <= 1200) {
+            quickAccessWidth = '50%';
+        }
+        else if (windowWidth > 1200) {
+            quickAccessWidth = '40%';
+        }
+        if (!quickAccessOpened) {
+            $('.quick-access').animate({
+                'width': quickAccessWidth,
+                'padding': '20px'
+            });
+
+        } else {
+            $('.quick-access').animate({
+                'width': '0',
+                'padding': '0'
+            });
+        }
+        $(this).toggleClass('actvie-quick-access');
+        quickAccessOpened = !quickAccessOpened;
+    });
 });
