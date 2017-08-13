1e37c5026389826b757e2a9fe85a49781a1bb880
Behzad Chizari <behzad75chizari@gmail.com>
2017-08-13 13:56:03 +0430

    Edit

--- js/custom/index.js
+++ js/custom/index.js
@@ -11,17 +11,25 @@ $.ajax({
     }
 });
 
-$(document).ready(function() {
-    $('.gotop').click(function() {
+$(document).ready(function () {
+    $('.gotop').click(function () {
         $('body').animatescroll();
     });
-    $('.center-heading h2').css('right', $('.center-heading').width() / 2 - $('.center-heading h2').width()/2);
-    $('.banner').css('height', $(window).height()+'px');
-    $(window).resize(function() {
+    $('.center-heading h2').css('right', $('.center-heading').width() / 2 - $('.center-heading h2').width() / 2);
+    $('.banner').css('height', $(window).height() + 'px');
+    $(window).resize(function () {
         $('.center-heading h2').css('right', $('.center-heading').width() / 2 - $('.center-heading h2').width() / 2);
     });
-
-
+    var isScrolled = false;
+    $(window).scroll(function () {
+        if (!isScrolled) {
+            $('#main').animatescroll({padding : 50});
+            isScrolled = true;
+        }
+        if ($(window).scrollTop() === 0) {
+            isScrolled = false;
+        }
+    });
 });
 
 
