0ce1d61586c28e766d74a30625ca5600c97df692
Behzad Chizari <behzad75chizari@gmail.com>
2017-08-29 11:17:26 +0430

    Edit

---
+++ js/custom/portal.js
@@ -0,0 +1,12 @@
+﻿$.ajax({
+    url: "../../../Pages/Template/g-nav.html",
+    success: function (result) {
+        document.getElementById('g-nav').innerHTML = result;
+    }
+});
+$.ajax({
+    url: "../../../Pages/Template/g-footer.html",
+    success: function (result) {
+        document.getElementById('g-footer').innerHTML = result;
+    }
+});
