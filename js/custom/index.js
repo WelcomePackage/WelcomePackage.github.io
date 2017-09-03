f2e0e44b39e69c8cc3b51d6061582bd504655261
Behzad Chizari <behzad75chizari@gmail.com>
2017-09-03 10:37:21 +0430

    Edit

--- js/custom/index.js
+++ js/custom/index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     var isScrolled = false;
     $(window).scroll(function () {
         if (!isScrolled) {
-            $('#main').animatescroll({padding : 50});
+            $('#main').animatescroll({ padding: 50 });
             isScrolled = true;
         }
         if ($(window).scrollTop() === 0) {
@@ -34,9 +34,44 @@ $(document).ready(function () {
         'bottom': 450 + 'px',
         //'right': 100+'px'
     });
-    $('.icon-box').click(function () {
-        
-        $(this).hide();
+    $('.btn-box-close').click(function () {
+        $('.icon-box-2').animate({ 'height': 0 + 'px' }, 1000);
+        //$('.icon-box-1').animate({ 'height': 200 + 'px' }, 1000);
+
+
+        $('.icon-box-1').css('display', 'none');
+        $('.icon-box-1').css('height', 'auto');
+        var htEng = $('.icon-box-1').css('height');
+        $('.icon-box-1').css('height', 0);
+        $('.icon-box-1').css('display', 'block');
+        $('.icon-box-1').animate({ 'height': htEng }, 1000);
+    });
+    $('.icon-box-1 a').click(function () {
+        $('.icon-box-1').animate({ 'height': 0 + 'px' }, 1000);
+        switch ($(this).attr('id')) {
+            case 'eng-bt':
+                {
+                    $('#eng-bx').css('display', 'none');
+                    $('#eng-bx').css('height', 'auto');
+                    var htEng = $('#eng-bx').css('height');
+                    $('#eng-bx').css('height', 0);
+                    $('#eng-bx').css('display', 'block');
+                    $('#eng-bx').animate({ 'height': htEng }, 1000);
+                    break;
+                }
+            case 'arch-bt':
+                {
+                    $('#arch-bx').css('display', 'none');
+                    $('#arch-bx').css('height', 'auto');
+                    var htArch = $('#arch-bx').css('height');
+                    $('#arch-bx').css('height', 0);
+                    $('#arch-bx').css('display', 'block');
+                    $('#arch-bx').animate({ 'height': htArch }, 1000);
+                    break;
+                }
+
+            default:
+        }
     });
 });
 
